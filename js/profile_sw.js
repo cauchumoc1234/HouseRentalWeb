f31a12f95f2ab7fd79004b4ecf05a843e726402b
cauchumoc1234 <ducanhfo3888@gmail.com>
2020-12-12 22:14:31 +0700

    Profile,loved room,account page

---
+++ js/profile_sw.js
@@ -0,0 +1,36 @@
+// control profile page switcher
+var notiPath = "noti-page.html";
+var profilePath = "profile-info.html";
+var likedRoomPath = "liked_rooms.html";
+$(document).ready(function(){
+    $("#acc-show").click(showProfile)
+    $("#noti-show").click(showNotification)
+    $("#liked-show").click(showLikedRooms)
+})
+function showProfile(){
+    for(var i of $(".account-nav li")){
+        if( $(i).attr("id") != "acc-show"){
+            $(i).removeClass("is-actived")
+        }
+    }
+    $("#acc-show").addClass("is-actived");
+    $("#mainFrame").attr("src",profilePath)
+}
+function showNotification(){
+    $("#noti-show").addClass("is-actived");
+    for(var i of $(".account-nav li")){
+        if( $(i).attr("id") != "noti-show"){
+            $(i).removeClass("is-actived")
+        }
+    }
+    $("#mainFrame").attr("src",notiPath)
+}
+function showLikedRooms(){
+    $("#liked-show").addClass("is-actived");
+    for(var i of $(".account-nav li")){
+        if( $(i).attr("id") != "liked-show"){
+            $(i).removeClass("is-actived")
+        }
+    }
+    $("#mainFrame").attr("src",likedRoomPath)
+}
